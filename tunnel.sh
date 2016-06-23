0dd85f259e3df779991f3c495f9811de50cc0bf1
Faizal Sadri <freakie2007@gmail.com>
2016-06-24 06:36:04 +0800

    Create tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+PORT=10006
+SERVER="1.32.58.108"
+REMOTEIP="127.0.0.1"
+REMOTEPORT=22
+OPTION="-R" # -R = Remote, -L = Local
+
+###
+# @function tunnel
+###
+function tunnel_ssh {
+    if [ "`ps -eaf | grep nNgf | grep $PORT | grep -v grep`" ] ; then
+        echo "Local Port tunnel to $REMOTE is up"
+    else
+        echo "Local Port tunnel ${REMOTE} NOT alive ... Restarting ..."
+        /usr/bin/ssh -nNgf $1@$2 $3 $4:$5:$6
+        logger -p daemon.notice "SSH Forward Local Port ${REMOTE} NOT Alive ... Restarting ..."
+        sleep 1
+    fi
+}
+
+##
+# Request User
+#
+case "$1" in
+    '-u')
+        USER=$2
+        if [ -z "$USER" ]; then
+             echo -e "Username Required.\n $(basename $0) [username]"
+        else
+            tunnel_ssh $USER $SERVER $OPTION $PORT $REMOTEIP $REMOTEPORT
+        fi
+    ;;
+    *)
+        echo "Usage: $(basename $0) [username]"
+    ;;
+esac
